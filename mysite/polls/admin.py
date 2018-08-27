1aa69247da74af70babb936c4c180387404becdc
TresLos <asistare7@gmail.com>
2018-08-27 19:26:18 +0200

    Capitol 7
    Aprenent a configurar el admin
    Toqueteixant el model

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -2,9 +2,20 @@ from django.contrib import admin
 
 from .models import Question, Choice
 
+# class ChoiceInline(admin.StackedInline): # Ocupa molt d'espai…
+class ChoiceInline(admin.TabularInline): # més compacte
+    model = Choice
+    extra = 3
 
 class QuestionAdmin(admin.ModelAdmin):
-    fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,               {'fields': ['question_text']}),
+        ('Date information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+    list_display = ('question_text', 'pub_date', 'was_published_recently')
+    list_filter = ['pub_date']
+
 
 admin.site.register(Question, QuestionAdmin)
 admin.site.register(Choice)
--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,3 +1,5 @@
+import datetime
+
 from django.db import models
 from django.utils import timezone
 
@@ -9,8 +11,12 @@ class Question(models.Model):
         return self.question_text
 
     def was_published_recently(self):
-        return (timezone.now() - self.pub_date).days == 0
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
+    was_published_recently.admin_order_field = 'pub_date'
+    was_published_recently.boolean = True
+    was_published_recently.short_description = 'Published recently?'
 
 
 class Choice(models.Model):
